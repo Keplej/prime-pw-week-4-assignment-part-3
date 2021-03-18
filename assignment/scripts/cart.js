82f11198268a44fc40516e148f73ced3b7fdc615
Johnny Keple <Johnny.Keple@gmail.com>
2021-03-18 10:55:54 -0500

    Commit #2

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,9 +5,23 @@ console.log('***** Cart Functions *****');
 //#1 global variable basket
 let basket = [];
 
-//#2 function addItem
+//#2 function addItem and adding it to the global variable
 function addItem(item) {
+  console.log(item);
   basket.push(item);
   return true;
 }
-console.log(basket);
+//Need double check if you need to have a return false;
+//checking if the function is working as intended
+addItem('item 1');
+addItem('Item 2');
+addItem('12');
+addItem();
+//Is this actually adding it to the global variable basket? Go back and check this
+
+//#3 List Items function with a loop
+function listItems(basket) {
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+}
