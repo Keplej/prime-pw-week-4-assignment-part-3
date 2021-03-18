c430b0cb2bfcdb3770a5bc9bbe22d4e9fee61bfe
Johnny Keple <Johnny.Keple@gmail.com>
2021-03-18 07:00:43 -0500

    First commit to get the assignment updated in github

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//#1 global variable basket
+let basket = [];
+
+//#2 function addItem
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+console.log(basket);
